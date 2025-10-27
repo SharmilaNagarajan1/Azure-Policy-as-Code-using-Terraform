88ffa82bdb589533b7fad602a767805a4393bcc5
sharmila <sharmeeh@gmail.com>
2025-10-27 17:17:34 +1100

    test2

---
+++ terraform-module/main.tf
@@ -0,0 +1,21 @@
+
+# Data source to get current subscription information
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "current" {}
+
+resource "azurerm_policy_definition" "restrict_location_policy" {
+  name         = var.policy_name
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = var.policy_display_name
+  policy_rule  = file("${path.module}/../policy-module/restrict-region-policy.json")
+}
+
+resource "azurerm_subscription_policy_assignment" "policy_assignmnet" {
+  name                   = var.policy_assignment_name
+  policy_definition_id   = azurerm_policy_definition.restrict_location_policy.id  
+  subscription_id        = data.azurerm_subscription.current.id
+  description            = var.policy_assignmnet_description
+  display_name           = var.policy_assignment_display_name
+}
